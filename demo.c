4ac80c3d66e47750d2ab9738882bddbc612a3256
Room1 FL.9a <essencesup02@9A-iMac-Classroom-3.local>
2025-01-24 13:46:09 +0700

    Branch demo

--- demo.c
+++ demo.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    printf("Y13 COM A! Message from another computer\n"); //Added commment
-
+    printf("Y13 COM A! Message from another computer\n"); //Added commment 
+    printf("This is created on the 13COM A branch\n");
 }
