e2a4f10006db6f5dfe874ff9f509378ecfc39465
Newton FL.9A room10 <essence06@Room10-FL9A.local>
2025-01-28 10:30:19 +0700

    This is a demo to link to #9
    This commit is done so that I can show the students of 12A how do I write a message that automatically link to issue #9 in GitHub.

--- demo.c
+++ demo.c
@@ -4,4 +4,5 @@ int main(void)
 {
     printf("Y13 COM B!\n"); //Added commment
     printf("All Y13 COM B students get into universities that you aim for. \n");
+    printf("I am going to commit this and link it to #9\n");
 }
