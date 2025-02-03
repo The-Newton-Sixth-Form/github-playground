6c37b61dce7208999bbeb3bd2b4c61f5f6637829
gh-test-archery <chuawei_jie@yahoo.com>
2025-02-03 01:24:48 +0000

    This is Chua testing the exercise.

--- demo.c
+++ demo.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
-    printf("Y13 COM A & B! Message from another computer\n"); //Added commment 
+    printf("Y13 COM A & B! Message from another computer\n"); //Added commment
     printf("This is created on the 13COM A branch and merged into the main\n");
     printf("All Y13 COM B students get into universities that you aim for. \n");
     printf("I am going to commit this and link it to #9\n");
+    printf("This is a test to make the exercise work.\n");
 }
